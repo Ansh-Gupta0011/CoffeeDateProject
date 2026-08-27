d9ac32b79897b38e3c0ba74fcc2f5f8630e88a0b
Ansh Gupta <117064396+Ansh-Gupta0011@users.noreply.github.com>
2026-08-27 17:37:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,20 @@
 require("dotenv").config();
 const app = require("./src/app");
-
 const coffeeDateRoutes = require(
     "./src/routes/coffeeDateRoutes"
 );
-
 const connectDatabase = require(
     "./src/config/database"
 );
-
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 async function startServer() {
     await connectDatabase();
-
     app.use(
         "/api/coffee-date",
         coffeeDateRoutes
     );
-
     app.listen(PORT, () => {
-        console.log(
-            `Server is running on http://localhost:${PORT}`
-        );
+        console.log(`Server is running on port ${PORT}`);
     });
 }
-
 startServer();
